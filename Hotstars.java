a2a6ed107b39680ba3fb43fac4e0273a85817f3d
unknown <darshannaik.xworkz.gmail.com>
2022-06-21 23:04:27 +0530

    array file

---
+++ Hotstars.java
@@ -0,0 +1,16 @@
+class Hotstars
+{
+public static void main(String a[])
+{
+String series[] = {"Special Ops","Eclipse","Aarya","The Empire","The Legend Hunuman","Live Telecast","1232KMS","Hundred","The Empire","Panipat","Alliance"};
+
+}
+
+static void series(String series[])
+ for (int i=0;i<series.length;i++){
+ System.out.println("series" +series[i]);}
+
+
+}
+
+
---
+++ SuperMarket.java
@@ -0,0 +1,135 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class SuperMarket 
+{
+public static void main(String a[])
+{
+String biscuits[] = {"Tiger","ParleG","Oreo","Mariegold","Unibic","Britania","sunfeast"};
+String veggies[] = { "Onoin","Tomato","Beetrot","Potato","Carrot","Coriandor","Mint"};
+String sportsProducts[] = {"Bat","Ball","SattleBat","Pad","Stump","Vollyball","football"};
+String grocerries[] = {"Mungdaal","Rava","Rice","Sugar","Chillypowder","Salt","Sesame"};
+String soaps[] = {"Hamam","Santoor","Mysoresandle","Dove","Medimix","Pearce","Lifebouy"};
+String detergents[] = {"Tide","Ariel","Nirma","Surf","Rin","Henko","Gain"};
+String coolDrinks[] = {"Cock","Thumbsup","7UP","Sprite","Pepsi","Fanta","Frooti"};
+String deos[] = {"Fogg","Axe","Nivea","Dove","sceret","Hugo","cinthol"};
+String homeApp[] = {"Tv","Ac","Microwave","Dishwasher","Pressurecooker","Refrigerator","Mixer","Washing machine"};
+String snacks[] = {"Popcorn","Trailmix","Chakli","Peanut","chips","Cashews","Chivda"};
+String fruits[] = {"Apple","Orenge","Mango","Banana","Avocado","Papaya","peach"};
+String chocolates[] = {"Snickers","Kitkat","Barone","Munch","Mars","Milkbar","Perk"};
+
+System.out.println("Biscuits1 " + biscuits[0]);
+System.out.println("Biscuits2 " + biscuits[1]);
+System.out.println("Biscuits3 " + biscuits[2]);
+System.out.println("Biscuits4 " + biscuits[3]);
+System.out.println("Biscuits5 " + biscuits[4]);
+System.out.println("Biscuits6 " + biscuits[5]);
+System.out.println("Biscuits7 " + biscuits[6]);
+
+System.out.println("Veggies1 " + veggies[0]);
+System.out.println("Veggies2 " + veggies[1]);
+System.out.println("Veggies3 " + veggies[2]);
+System.out.println("Veggies4 " + veggies[3]);
+System.out.println("Veggies5 " + veggies[4]);
+System.out.println("Veggies6 " + veggies[5]);
+System.out.println("Veggies7 " + veggies[6]);
+
+System.out.println("SportsProducts1 " + sportsProducts[0]);
+System.out.println("SportsProducts2 " + sportsProducts[1]);
+System.out.println("SportsProducts3 " + sportsProducts[2]);
+System.out.println("SportsProducts4 " + sportsProducts[3]);
+System.out.println("SportsProducts5 " + sportsProducts[4]);
+System.out.println("SportsProducts6 " + sportsProducts[5]);
+System.out.println("SportsProducts7 " + sportsProducts[6]);
+
+System.out.println("Glocerries1 " + grocerries[0]);
+System.out.println("Glocerries2 " + grocerries[1]);
+System.out.println("Glocerries3 " + grocerries[2]);
+System.out.println("Glocerries4 " + grocerries[3]);
+System.out.println("Glocerries5 " + grocerries[4]);
+System.out.println("Glocerries6 " + grocerries[5]);
+System.out.println("Glocerries7 " + grocerries[6]);
+
+System.out.println("Soaps1 " + soaps[0]);
+System.out.println("Soaps2 " + soaps[1]);
+System.out.println("Soaps3 " + soaps[2]);
+System.out.println("Soaps4 " + soaps[3]);
+System.out.println("Soaps5 " + soaps[4]);
+System.out.println("Soaps6 " + soaps[5]);
+System.out.println("Soaps7 " + soaps[6]);
+
+System.out.println("Detargents1 " + detergents[0]);
+System.out.println("Detargents2 " + detergents[1]);
+System.out.println("Detargents3 " + detergents[2]);
+System.out.println("Detargents4 " + detergents[3]);
+System.out.println("Detargents5 " + detergents[4]);
+System.out.println("Detargents6 " + detergents[5]);
+System.out.println("Detargents7 " + detergents[6]);
+
+System.out.println("CoolDrinks1 " + coolDrinks[0]);
+System.out.println("CoolDrinks2 " + coolDrinks[1]);
+System.out.println("CoolDrinks3 " + coolDrinks[2]);
+System.out.println("CoolDrinks4 " + coolDrinks[3]);
+System.out.println("CoolDrinks5 " + coolDrinks[4]);
+System.out.println("CoolDrinks6 " + coolDrinks[5]);
+System.out.println("CoolDrinks7 " + coolDrinks[6]);
+
+System.out.println("Deos1 " + deos[0]);
+System.out.println("Deos2 " + deos[1]);
+System.out.println("Deos3 " + deos[2]);
+System.out.println("Deos4 " + deos[3]);
+System.out.println("Deos5 " + deos[4]);
+System.out.println("Deos6 " + deos[5]);
+System.out.println("Deos7 " + deos[6]);
+
+System.out.println("HomeApp1 " + homeApp[0]);
+System.out.println("HomeApp2 " + homeApp[1]);
+System.out.println("HomeApp3 " + homeApp[2]);
+System.out.println("HomeApp4 " + homeApp[3]);
+System.out.println("HomeApp5 " + homeApp[4]);
+System.out.println("HomeApp6 " + homeApp[5]);
+System.out.println("HomeApp7 " + homeApp[6]);
+
+System.out.println("Sncks1 " + snacks[0]);
+System.out.println("Sncks2 " + snacks[1]);
+System.out.println("Sncks3 " + snacks[2]);
+System.out.println("Sncks4 " + snacks[3]);
+System.out.println("Sncks5 " + snacks[4]);
+System.out.println("Sncks6 " + snacks[5]);
+System.out.println("Sncks7 " + snacks[6]);
+
+System.out.println("Fruits1 " + fruits[0]);
+System.out.println("Fruits2 " + fruits[1]);
+System.out.println("Fruits3 " + fruits[2]);
+System.out.println("Fruits4 " + fruits[3]);
+System.out.println("Fruits5 " + fruits[4]);
+System.out.println("Fruits6 " + fruits[5]);
+System.out.println("Fruits7 " + fruits[6]);
+
+System.out.println("Chocolets1 " + chocolates[0]);
+System.out.println("Chocolets2 " + chocolates[1]);
+System.out.println("Chocolets3 " + chocolates[2]);
+System.out.println("Chocolets4 " + chocolates[3]);
+System.out.println("Chocolets5 " + chocolates[4]);
+System.out.println("Chocolets6 " + chocolates[5]);
+System.out.println("Chocolets7 " + chocolates[6]);
+
+}
+
+
+
+
+
+}
