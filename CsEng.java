83c81dc5915bd7c67fb34a029425b35401f45f3f
unknown <darshannaik.xworkz.gmail.com>
2022-06-28 08:51:03 +0530

    File Uploaded

---
+++ CsEng.java
@@ -0,0 +1,7 @@
+class CsEng extends Eng{
+  static String job;
+
+public void developerOrTester(){
+System.out.println("The main purpose of this engineering is "+job);
+}  
+}
---
+++ Eng.java
@@ -0,0 +1,9 @@
+class Eng
+{
+static String field;
+public void problemSolving()
+{
+ System.out.println("Problem solved by an Enineer ");
+} 
+
+}
